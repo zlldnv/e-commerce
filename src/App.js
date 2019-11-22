c545d4274929d3e17afc3aa7e2892c6c7e637a78
Anton <zalaldinov@icloud.com>
2019-11-22 14:13:49 +0300

    make menu layout

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./Homepage";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage  ></HomePage>
+    </div>
+  );
+}
+
+export default App;
